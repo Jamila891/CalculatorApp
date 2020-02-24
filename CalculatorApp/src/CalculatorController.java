1df5646161017a12c928ceee9c8fedc7d9d42301
jamilamahler <jamilamahler@macbookpro.karakun.com>
2020-02-24 08:25:31 +0100

    new methods in Controller (Array not initialized)

--- CalculatorApp/src/CalculatorController.java
+++ CalculatorApp/src/CalculatorController.java
@@ -7,6 +7,8 @@ public class CalculatorController {
 	private final CalculatorModel model;
 	private final CalculatorView view;
 	
+	
+	
 	protected CalculatorController(CalculatorModel model, CalculatorView view) {
 		this.model = model;
 		this.view = view;
@@ -16,8 +18,34 @@ public class CalculatorController {
 		Button b = new Button();
 		b = (Button) e.getSource();
 		
-		if (b.equals(view.num1)) {
+		if (b == view.num1) {
 			view.numbersSum.setText("1");
 		}
+	}
+	
+	public void clearAll (Event e) {
+		
+	}
+	
+	public int getResult(String input) {
+		String[] numbersString;
+		int[] numbersIntegers;
+		int sum, num;
+		
+		numbersString = input.split("\\+");
+		
+		for (int i = 0; i < numbersString.length;i++) {
+			num = Integer.parseInt(numbersString[i]);
+			
+			for (int j = 0; i<numbersIntegers.length;j++) {
+				num = numbersIntegers[j];
+			}
+		}
+		
+		for (int x = 0; x<numbersIntegers.length; x++) {
+			sum += numbersIntegers[x];
+		}
+		
+		return sum;
 	}
 }
